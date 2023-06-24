01b9799091838f5f3f04fa438e4a31fe42edf41e
SzymCode <s.radomski19@gmail.com>
2023-06-24 19:41:59 +0200

    Preview mobile menu

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -7,8 +7,7 @@ import { BsCart } from "react-icons/bs"
 import { BiMenuAltRight } from "react-icons/bi"
 import { VscChromeClose } from "react-icons/vsc"
 
-import Wrapper from "./Wrapper.jsx"
-import Menu from "./Menu.jsx"
+import { Menu, MenuMobile, Wrapper } from "@/components"
 
 const Header = () => {
   const [mobileMenu, setMobileMenu] = useState(false)
@@ -19,12 +18,44 @@ const Header = () => {
 
   return (
     <header className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}>
-      <Wrapper className="flex items-center justify-between w-full">
+      <Wrapper className="h-[60px] flex justify-between items-center">
         <Link href="/">
           <Image src="/ecommercetemplate_logo.png" alt="logo" width="260" height="50"/>
         </Link>
-        <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} categories={categories}/>
 
+        <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} categories={categories}/>
+        {mobileMenu && (
+          <MenuMobile showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} setMobileMenu={setMobileMenu} categories={categories}/>
+        )}
+
+
+        <div className="flex items-center gap-2 text-black">
+
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+            <IoMdHeartEmpty className="text-[19px] md:text-[24px]" />
+            <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+              12
+            </div>
+          </div>
+
+          <Link href="/cart">
+            <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+              <BsCart className="text-[15px] md:text-[20px]" />
+              <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+                21
+              </div>
+            </div>
+          </Link>
+
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex md:hidden justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2">
+            {mobileMenu ? (
+              <VscChromeClose className="text-[16px]" onClick={() => setMobileMenu(false)} />
+            ) : (
+              <BiMenuAltRight className="text-[20px]" onClick={() => setMobileMenu(true)} />
+            )}
+          </div>
+
+        </div>
       </Wrapper>
     </header>
   )
