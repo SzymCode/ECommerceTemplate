6e6ee59dee86c454ddfbabf037468e45f411a5d8
SzymCode <s.radomski19@gmail.com>
2023-06-25 21:35:48 +0200

    Initialize backend with npx create-strapi-app@latest

--- frontend/components/RelatedProducts.jsx
+++ frontend/components/RelatedProducts.jsx
@@ -4,6 +4,7 @@ import Carousel from "react-multi-carousel"
 import "react-multi-carousel/lib/styles.css"
 import ProductCard from "./ProductCard"
 
+
 const RelatedProducts = () => {
   const productCards = Array.from(Array(40), (_, index) => (<ProductCard key={index} condition={(index % 3) + 1} />))
   const responsive = {
