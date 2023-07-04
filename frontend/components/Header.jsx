c64654c0465560cc01c21f6a7156c34399f26826
SzymCode <s.radomski19@gmail.com>
2023-07-04 22:07:36 +0200

    Update logo

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
     <header className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}>
       <Wrapper className="h-[60px] flex justify-between items-center">
         <Link href="/">
-          <Image src="/ecommercetemplate_logo.png" alt="logo" width="260" height="50"/>
+          <Image src="/logo.png" alt="logo" width="260" height="50"/>
         </Link>
 
         <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} categories={categories}/>
